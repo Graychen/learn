9a47c4294b29f2539e91d93672361081a6f2e7bf
Graychen <13780185250@sina.com>
2016-06-27 17:48:03 +0800

    添加mainwenjian

---
+++ project/app/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import React from 'react-router';
+import React from ./routes';
+
+Router.run(router                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ls
+                    git status
+                    
+                    
+                    
+                    
+                    
+                    
+                    ;
