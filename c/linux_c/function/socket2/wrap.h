9d2b6b6b0a11a4d566bc8dd5337df9dd6a22b0fe
Graychen <13780185250@sina.cn>
2017-02-09 17:25:38 +0800

    socket学习

---
+++ c/linux_c/function/socket2/wrap.h
@@ -0,0 +1,11 @@
+void perr_exit(const char *s);
+int Accept(int fd,struct sockaddr *sa,socklen_t *salenptr);
+void Bind(int fd,const struct sockaddr *sa,socklen_t salen);
+void Connect(int fd,const struct sockaddr *sa,socklen_t salen);
+void Listen(int fd,int backlog);
+int Socket(int family,int type,int protocol);
+ssize_t Read(int fd,void *ptr,size_t nbytes);
+ssize_t Write(int fd,const void *ptr,size_t nbytes);
+static ssize_t my_read(int fd,char *ptr);
+ssize_t Readline(int fd,void *vptr,size_t maxlen);
+void Close(int fd);
